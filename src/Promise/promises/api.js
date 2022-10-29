237a7d345276fa98c52ec454284f6e5b4c068497
KauhareniaNastassia <kavgorenya_a@tut.by>
2022-10-30 00:37:33 +0300

    add Promise practise

---
+++ src/Promise/promises/api.js
@@ -0,0 +1,21 @@
+const api = {
+    sendStudentsCountToItKamasutra(studentsCount) {
+        return axios.post("https://it-kamasutra.com/students-need",
+            {count: studentsCount});
+    },
+    getVacanciesCountFromMicrosoft() {
+        const pr = axios.get("https://microsoft.com");
+
+        let pr2 = pr.then(res => {
+            return res.data.vacancies
+        })
+        return pr2
+
+    },
+    getVacanciesCountFromGoogle() {
+        return axios
+            .get("https://google.com")
+            .then( res => res.data.vacancies )
+    }
+
+}
